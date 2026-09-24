04a8834ee812a0b274454331eac757ded7aba1b0
Lenovo <Lenovo@DESKTOP-SNANKA1>
2026-09-24 14:52:19 +0700

    S4 S5

--- Web2_API/Program.cs
+++ Web2_API/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IBookRepository, SQLBookRepository>();
+builder.Services.AddScoped<IAuthorRepository, SQLAuthorRepository>();
 // Register DbContext
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options =>
